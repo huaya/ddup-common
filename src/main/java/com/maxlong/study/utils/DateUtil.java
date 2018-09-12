3e9b0566813bd27c861bd4ad483de67c7e7546cf
ma.xl <maxl@gjmetal.com>
2018-09-12 18:30:21 +0800

    dbmanger

--- src/main/java/com/maxlong/study/utils/DateUtil.java
+++ src/main/java/com/maxlong/study/utils/DateUtil.java
@@ -1,10 +1,13 @@
 package com.maxlong.study.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * This class consist some static mothod to operate date.
+ *
  * @author： ma.xl
  * @dateTime： 2018-8-31 9:46
  */
@@ -12,12 +15,60 @@ public class DateUtil {
 
     /**
      * convert a date to string in a specifies fromat.
+     *
      * @param date
      * @param dateFormat
      * @return
      */
-    public static String dateToStr(Date date, String dateFormat){
+    public static String dateToStr(Date date, String dateFormat) {
         SimpleDateFormat myFormat = new SimpleDateFormat(dateFormat);
         return myFormat.format(date);
+    }
+
+    /**
+     * parse a String to Date in a specifies fromat.
+     *
+     * @param dateStr
+     * @param dateFormat
+     * @return
+     * @throws ParseException
+     */
+    public static Date strToDate(String dateStr, String dateFormat) {
+        SimpleDateFormat myFormat = new SimpleDateFormat(dateFormat);
+        try {
+            return myFormat.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     * date add Second
+     *
+     * @param date
+     * @param num
+     * @return
+     */
+    public static Date addSecond(Date date, int num) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.SECOND, num);
+        return calendar.getTime();
+    }
+
+    /**
+     * date add Second return String
+     *
+     * @param date
+     * @param num
+     * @return
+     */
+    public static String addSecond(Date date, int num, String dateFormat) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.SECOND, num);
+        return dateToStr(calendar.getTime(), dateFormat);
     }
 }
