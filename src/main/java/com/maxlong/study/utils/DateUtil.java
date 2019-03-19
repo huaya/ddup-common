6fa08e90567a284f806e0258066b49a423273783
ma.xl <maxl@gjmetal.com>
2019-03-19 14:23:27 +0800

    commit

--- src/main/java/com/maxlong/study/utils/DateUtil.java
+++ src/main/java/com/maxlong/study/utils/DateUtil.java
@@ -93,4 +93,30 @@ public class DateUtil {
     public static String longToDate(long dateLong, String dateFormat) {
         return dateToStr(new Date(dateLong), dateFormat);
     }
+
+    /**
+     * compare two date String with a pattern
+     * @param date1
+     * @param date2
+     * @param pattern
+     * @return
+     */
+    public static int compareDate(String date1, String date2, String pattern) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        try {
+            Date dt1 = dateFormat.parse(date1);
+            Date dt2 = dateFormat.parse(date2);
+            if(dt1.getTime() > dt2.getTime()){
+                return 1;
+            }else if(dt1.getTime() < dt2.getTime()){
+                return -1;
+            }else {
+                return 0;
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return 	0;
+        }
+    }
 }
